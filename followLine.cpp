321942e134ca1892c1ca7e87fd3bee3d6238a6e3
GregoryMason <GregoryMason@users.noreply.github.com>
2017-05-09 13:02:02 +1200

    Update followLine.cpp

--- followLine.cpp
+++ followLine.cpp
@@ -5,27 +5,28 @@
  *(one row above, and one row below the passed row int)
  *int row must be between 1 and 238
  */
-int getRow(int row){
-	int rowsAv[];
+void getRow(int row){
+	int rowsAv[32];
 	int pixelTotal = 0; 	//Total value of the pixels from all 3 rows
-	int average; 	    	// will hold the average value of the three pixels
-	int linePlace[];	// int array of ---
-	int max = 1;		// maximum value of ---
+	int average; 	    	//average value of the three pixels
+	int max = 100;
 	
 	for (int pixel = 0; pixel < 320; pixel += 10) { //Get's every 4th pixel
-		pixelTotal = get_pixel(pixel,row-1,3) + get_pixel(pixel,row,3) + get_pixel(pixel,row+1,3);
+		pixelTotal = get_pixel(row-1,pixel,3) + get_pixel(row,pixel,3) + get_pixel(row+1,pixel,3);
 		average = pixelTotal/3;
 		rowsAv[pixelRow] = average;
 		if (average > max) max = average;
 	}
 	
-	int threshold = max / 2; //threshold of --- 
+	int threshold = max / 2;
 	
 	for (int pixel = 0; pixel < 32; pixel++) {
-		if (rowsAv[pixel] < threshold) linePlace[pixel] = 1;
+		if (rowsAv[pixel] > threshold) {
+			linePlace[pixel] = 1;
+		} else {
+			linePlace[pixel] = 0;
+		}
 	}
-	
-	return linePlace;
 }
 
 /*Detects where the white line is in relation to the centre of the robot.
@@ -35,9 +36,9 @@ int getRow(int row){
 double detectLine(){
 	//take a picture
 	take_picture();
-	display_picture();
+	display_picture(1,0);
 	
-	int[] linePlace = getRow(200);
+	getRow(200);
 	
 	double error = 0;
 	int numPixels = 0;
@@ -49,7 +50,5 @@ double detectLine(){
 	}
 	if (numPixels > 0) {
 		return (error/numPixels);
-	} else {
-		return 0;
 	}
-}
+return 0;}
